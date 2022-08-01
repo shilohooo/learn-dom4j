5c14f618c57259bfaa0ed14337437f282428000e
shiloh <lixiaolei595@gmail.com>
2022-08-01 18:35:25 +0800

    feat: 新增符合常量

---
+++ src/main/java/org/shiloh/common/constant/SymbolConstants.java
@@ -0,0 +1,17 @@
+package org.shiloh.common.constant;
+
+/**
+ * 符号常量
+ *
+ * @author shiloh
+ * @date 2022/8/1 18:29
+ */
+public final class SymbolConstants {
+    private SymbolConstants() {
+    }
+
+    /**
+     * 左大括号
+     */
+    public static final String LEFT_BRACE = "(";
+}
